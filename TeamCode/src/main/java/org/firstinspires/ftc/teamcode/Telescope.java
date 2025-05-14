bbfae384b49f47918e005b69c6534002cf4f1f37
SycaTech <sycatech18268.2@gmail.com>
2025-05-14 17:47:10 +0300

    added telescope files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telescope.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode;
+
+import java.util.function.DoubleSupplier;
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+public class Telescope  extends SubsystemBase {
+        private final Telemetry telemetry;
+        private final DcMotorEx elevatorMotor;
+        private static double currentPosition;
+
+        private final PIDController pid = new PIDController(
+                TelescopeConstants.LiftConstants.KP,
+                TelescopeConstants.LiftConstants.KI,
+                TelescopeConstants.LiftConstants.KD
+        );
+        public static boolean isUsingPID = true;
+
+        public Telescope(Telemetry telemetry, HardwareMap hardwareMap) {
+            this.telemetry = telemetry;
+
+            elevatorMotor = hardwareMap.get(DcMotorEx.class, TelescopeConstants.LiftConstants.ELEVATOR_MOTOR);
+            elevatorMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+            elevatorMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            elevatorMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            pid.setTolerance(TelescopeConstants.LiftConstants.TOLERANCE);
+
+        }
+
+        public void update() {
+            double output = pid.calculate(elevatorMotor.getCurrentPosition());
+
+            elevatorMotor.setPower(output);
+        }
+
+        public Command setSetPoint(double setPoint) {
+            return new InstantCommand(() -> pid.setSetPoint(setPoint), this);
+        }
+
+        public Command setSetPoint(DoubleSupplier setPoint) {
+            return new InstantCommand(() -> pid.setSetPoint(setPoint.getAsDouble()), this);
+        }
+
+        public void add(double add) {
+            pid.setSetPoint(pid.getSetPoint() + add);
+        }
+
+        public double getSetpoint() {
+            return pid.getSetPoint();
+        }
+
+        public Command defaultCommand() {
+            return new RunCommand(() -> pid.setSetPoint(TelescopeConstants.LiftConstants.DEFAULT_POSITION2), this);
+        }
+
+        public Command unUsePID() {
+            return new InstantCommand(() -> isUsingPID = false);
+        }
+
+        public Command climbePower() {
+            if (!isUsingPID) {
+                return new RunCommand(() -> elevatorMotor.setPower(-0.5));
+            }
+            return new InstantCommand();
+
+        }
+
+        public boolean isAtSetPoint() {
+            return pid.atSetPoint();
+        }
+
+        public static double getHeight() {
+            return (currentPosition * TelescopeConstants.LiftConstants.TICK_TO_CM) + TelescopeConstants.LiftConstants.STARTING_HEIGHT;
+        }
+
+        @Override
+        public void periodic() {
+            if (isUsingPID){
+                update();
+            }
+
+            currentPosition = elevatorMotor.getCurrentPosition();
+
+            telemetry.addData("Lift SetPoint", pid.getSetPoint());
+            telemetry.addData("Lift Position", elevatorMotor.getCurrentPosition());
+            telemetry.addData("Lift At Set Point", pid.atSetPoint());
+            telemetry.addData("Lift Power" , elevatorMotor.getPower());
+            telemetry.addData("Lift Height", getHeight());
+
+            telemetry.update();
+            pid.setSetPoint(pid.getSetPoint());
+
+        }
+
+    }
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelescopeTeleop.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class TelescopeTeleop extends CommandOpMode {
+        @Override
+        public void initialize() {
+            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+            GamepadEx controller = new GamepadEx(gamepad1);
+
+            Telescope telescope = new Telescope(telemetry, hardwareMap);
+
+            telescope.setDefaultCommand(telescope.defaultCommand());
+
+            controller.getGamepadButton(GamepadKeys.Button.DPAD_UP).whileActiveContinuous(telescope.setSetPoint(TelescopeConstants.LiftConstants.LOW_BASKET));
+            controller.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whileActiveContinuous(telescope.setSetPoint(TelescopeConstants.LiftConstants.HIGH_BASKET));
+            controller.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whileActiveContinuous(telescope.setSetPoint(TelescopeConstants.LiftConstants.SCORE_LOW_SPECIMEN));
+            controller.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whileActiveContinuous(telescope.setSetPoint(TelescopeConstants.LiftConstants.PRE_SCORE_HIGH_SPECIMEN));
+        }
+    }
+
