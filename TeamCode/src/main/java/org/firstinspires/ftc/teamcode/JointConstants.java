a5f7fbd9ec6666a1f1a3ab61a8ebb89fe124e1eb
SycaTech <sycatech18268.2@gmail.com>
2025-05-19 20:18:33 +0300

    INTAKE CHANGES

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JointConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JointConstants.java
@@ -6,34 +6,10 @@ public class JointConstants {
     @Config
     public static class jointConstants {
         public static final String RIGHT_ANGLE_SERVO = "Angle Right";
-        public static final String RIGHT_ROLLER = "Roller Right";
         public static final String LEFT_ANGLE_SERVO = "Angle Left";
-        public static final String LEFT_ROLLER = "Roller Left";
-        public static final String DISTANCE_SENSOR = "color sensor";
-
-        public static double INTAKE_POWER = 0.9;
-        public static double OUTTAKE_POWER = -0.6;
-        public static double ZERO_POWER = 0;
 
         public static double DEFAULT_POSITION = 0.1;
         public static double DEFAULT_POSITION2 = 0.5;
-        public static double INTAKE_DEFAULT = 1;
-        public static double INTAKE_DEFAULT2 = 0.1;
 
-        public static double PRE_SCORE_SPECIMEN_LOW = 0.5;
-        public static double PRE_SCORE_SPECIMEN_HIGH = 0;
-
-        public static double OBSERVATION_OUTTAKE = 0.5;
-
-        public static double INTAKE_SPECIMEN_POSITION = 0.55;
-        public static double INTAKE_SAMPLE_POSITION = 0.9;
-        public static double EXIT_SUBMERSIBLE = 0.3;
-
-        public static double SCORE_BASKET_LOW = 1;
-        public static double DEFAULT_INTAKE = 0.15;
-        public static double SCORE_BASKET_HIGH = 0.35;
-
-        public static double SCORE_SPECIMEN_LOW = 0.5;
-        public static double SCORE_SPECIMEN_HIGH = 0.9;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Teleop {
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class Teleop extends CommandOpMode{
+
+    @Override
+    public void initialize() {
+            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+            GamepadEx controller = new GamepadEx(gamepad1);
+
+            IntakeJoint intakeJoint = new IntakeJoint(hardwareMap,telemetry);
+
+            intakeJoint.setDefaultCommand(intakeJoint.defaultCommand());
+
+            controller.getGamepadButton(GamepadKeys.Button.DPAD_UP).whileActiveContinuous(intakeJoint.defaultCommand());
+            controller.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whileActiveContinuous(intakeJoint.intakeSubmersible());
+        }
 }
