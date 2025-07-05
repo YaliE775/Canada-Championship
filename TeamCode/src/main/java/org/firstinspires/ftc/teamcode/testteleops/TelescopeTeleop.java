1690fec129b9d0fa848de0ae7b5ede2c2b08a358
SycaTech <sycatech18268.2@gmail.com>
2025-07-05 22:11:25 +0300

    Update TelescopeTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testteleops/TelescopeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testteleops/TelescopeTeleop.java
@@ -12,19 +12,19 @@ import org.firstinspires.ftc.teamcode.Telescope;
 
 @TeleOp
 public class TelescopeTeleop extends CommandOpMode {
-        @Override
-        public void initialize() {
-            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    @Override
+    public void initialize() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-            GamepadEx controller = new GamepadEx(gamepad1);
-            Telescope telescope = new Telescope(telemetry, hardwareMap);
+        GamepadEx controller = new GamepadEx(gamepad1);
+        Telescope telescope = new Telescope(telemetry, hardwareMap);
 
-            telescope.setDefaultCommand(telescope.defaultCommand());
+        telescope.setDefaultCommand(telescope.defaultCommand());
 
-            controller.getGamepadButton(GamepadKeys.Button.DPAD_UP).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.LOW_BASKET));
-            controller.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.HIGH_BASKET));
-            controller.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.SCORE_LOW_SPECIMEN));
-            controller.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.PRE_SCORE_HIGH_SPECIMEN));
-        }
+        controller.getGamepadButton(GamepadKeys.Button.DPAD_UP).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.LOW_BASKET));
+        controller.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.HIGH_BASKET));
+        controller.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.SCORE_LOW_SPECIMEN));
+        controller.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whileActiveContinuous(telescope.setSetPoint(Constants.TelescopeConstants.PRE_SCORE_HIGH_SPECIMEN));
     }
+}
 
