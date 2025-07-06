7927f8819b64cae014bee9a900312907e3b5bbdf
SycaTech <sycatech18268.2@gmail.com>
2025-07-06 20:38:48 +0300

    added motor test class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,37 +2,38 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.teamcode.subsystems.Drive;
-
+@TeleOp
 public class Teleop extends CommandOpMode {
     private GamepadEx driverController;
+    private DcMotorEx motor;
 
     @Override
-        public void initialize() {
-            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    public void initialize() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        Drive drive = new Drive(hardwareMap, telemetry);
+        motor = hardwareMap.get(DcMotorEx.class, "1");
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-            driverController = new GamepadEx(gamepad1);
+        driverController = new GamepadEx(gamepad1);
 
-            drive.setDefaultCommand(
-                    drive.driveRobotCentric(
-                            ()-> -driverController.getLeftY(),
-                            ()-> -driverController.getLeftX(),
-                            ()-> -driverController.getRightX())
-            );
-
-            driverController.getGamepadButton(GamepadKeys.Button.B).whenActive(drive.setDriveAngle(90));
-            driverController.getGamepadButton(GamepadKeys.Button.A).whenActive(drive.resetIMU());
-
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            telemetry.update();
-        }
+        driverController.getGamepadButton(GamepadKeys.Button.B).whileActiveContinuous(new RunCommand(() -> motor.setPower(0.5)));
+        driverController.getGamepadButton(GamepadKeys.Button.X).whileActiveContinuous(new RunCommand(() -> motor.setPower(-0.5)));
+        driverController.getGamepadButton(GamepadKeys.Button.Y).whileActiveContinuous(new RunCommand(() -> motor.setPower(0)));
     }
+
+    @Override
+    public void run() {
+        super.run();
+        telemetry.addData("Encder Value: ", motor.getCurrentPosition());
+        telemetry.addData("Power: ", motor.getPower());
+        telemetry.update();
+    }
+
+}
